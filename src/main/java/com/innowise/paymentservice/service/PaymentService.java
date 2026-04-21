2bec8a66ba9c45e3395aeaaf86b41a2677b200b1
lexavodoprovod <nikitoshololeenko18@gmail.com>
2026-04-22 02:04:03 +0300

    feat: add PaymentService contract

--- src/main/java/com/innowise/paymentservice/service/PaymentService.java
+++ src/main/java/com/innowise/paymentservice/service/PaymentService.java
@@ -1,4 +1,22 @@
 package com.innowise.paymentservice.service;
 
+import com.innowise.paymentservice.dto.request.PaymentRequestDto;
+import com.innowise.paymentservice.dto.response.PaymentResponseDto;
+import com.innowise.paymentservice.entity.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface PaymentService {
+
+    PaymentResponseDto createPayment(PaymentRequestDto paymentRequestDto);
+
+    PaymentResponseDto findPaymentById(String paymentId);
+
+    Page<PaymentResponseDto> findAllPayments(Pageable pageable);
+
+    Page<PaymentResponseDto> findPaymentsByUserIdOrOrderIdOrStatus(Long userId, Long orderId, String status, Pageable pageable);
+
+    PaymentResponseDto changePaymentStatus(String paymentId, Status status);
+
+    Boolean softDeletePayment(String paymentId);
 }
--- src/main/java/com/innowise/paymentservice/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/innowise/paymentservice/service/impl/PaymentServiceImpl.java
@@ -1,4 +1,52 @@
 package com.innowise.paymentservice.service.impl;
 
-public class PaymentServiceImpl {
+import com.innowise.paymentservice.dto.request.PaymentRequestDto;
+import com.innowise.paymentservice.dto.response.PaymentResponseDto;
+import com.innowise.paymentservice.entity.Status;
+import com.innowise.paymentservice.mapper.PaymentMapper;
+import com.innowise.paymentservice.repository.PaymentRepository;
+import com.innowise.paymentservice.repository.impl.CustomPaymentRepositoryImpl;
+import com.innowise.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final CustomPaymentRepositoryImpl customPaymentRepository;
+    private final PaymentMapper paymentMapper;
+
+    @Override
+    public PaymentResponseDto createPayment(PaymentRequestDto paymentRequestDto) {
+        return null;
+    }
+
+    @Override
+    public PaymentResponseDto findPaymentById(String paymentId) {
+        return null;
+    }
+
+    @Override
+    public Page<PaymentResponseDto> findAllPayments(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<PaymentResponseDto> findPaymentsByUserIdOrOrderIdOrStatus(Long userId, Long orderId, String status, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public PaymentResponseDto changePaymentStatus(String paymentId, Status status) {
+        return null;
+    }
+
+    @Override
+    public Boolean softDeletePayment(String paymentId) {
+        return null;
+    }
 }
