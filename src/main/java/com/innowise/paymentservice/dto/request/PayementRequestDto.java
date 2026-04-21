ecb9e6a37df1d9a83b9803a00bffbcf7eac194a2
lexavodoprovod <nikitoshololeenko18@gmail.com>
2026-04-22 01:08:24 +0300

    feat: add PaymentRequestDto

--- src/main/java/com/innowise/paymentservice/dto/request/PayementRequestDto.java
+++ src/main/java/com/innowise/paymentservice/dto/request/PayementRequestDto.java
@@ -1,4 +1,16 @@
 package com.innowise.paymentservice.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public class PayementRequestDto {
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long orderId;
+
+    @NotNull
+    private Long paymentAmount;
+
 }
