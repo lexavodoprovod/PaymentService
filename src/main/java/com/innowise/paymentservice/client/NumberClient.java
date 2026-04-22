94db86331e98f957c292e12c505f0f7b89401591
lexavodoprovod <nikitoshololeenko18@gmail.com>
2026-04-22 03:07:06 +0300

    feat: add connection with external API

--- src/main/java/com/innowise/paymentservice/client/NumberClient.java
+++ src/main/java/com/innowise/paymentservice/client/NumberClient.java
@@ -1,4 +1,22 @@
 package com.innowise.paymentservice.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(
+        name = "random-number-api",
+        url = "http://www.randomnumberapi.com/api/v1.0",
+        path = "/random"
+)
 public interface NumberClient {
+
+    @GetMapping()
+    List<Integer> getRandomNumber(
+            @RequestParam int min,
+            @RequestParam int max,
+            @RequestParam int count
+    );
 }
