42926c97f015130814d859d729da5a54abe48644
lexavodoprovod <nikitoshololeenko18@gmail.com>
2026-05-09 07:02:57 +0300

    refactor: remove useless method from BaseIT

--- src/test/java/com/innowise/paymentservice/controller/BaseIT.java
+++ src/test/java/com/innowise/paymentservice/controller/BaseIT.java
@@ -1,17 +1,10 @@
 package com.innowise.paymentservice.controller;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
-import com.innowise.paymentservice.dto.kafkadto.PaymentEventDto;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -21,7 +14,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
-import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
@@ -49,22 +41,4 @@ public abstract class BaseIT {
         registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
     }
 
-//    protected Consumer<String, PaymentEventDto> createTestConsumer() {
-//        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps(
-//                kafka.getBootstrapServers(),
-//                "test-group",
-//                "true"
-//        );
-//
-//        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-//
-//        JsonDeserializer<PaymentEventDto> jsonDeserializer = new JsonDeserializer<>(PaymentEventDto.class);
-//        jsonDeserializer.addTrustedPackages("*");
-//
-//        return new DefaultKafkaConsumerFactory<>(
-//                consumerProps,
-//                new StringDeserializer(),
-//                jsonDeserializer
-//        ).createConsumer();
-//    }
 }
